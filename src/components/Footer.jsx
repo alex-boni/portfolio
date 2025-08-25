ead024852c81510625c2d984f6852ea4add713cd
alexboni97 <alexboni@ucm.es>
2025-08-25 23:54:00 +0200

    add source code footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,13 @@ function Footer() {
   return (
     <>
       <footer className="bg-gray-900 p-8 ">
-        <p className="mb-2 text-white/70">&copy; 2025 Alex Guillermo Bonilla Taco</p>
+        <p className=" text-white/70">
+          &copy; 2025 Alex Guillermo Bonilla Taco
+        </p>
+        <p className="mb-6 text-white/70">
+          Este portfolio está desarrollado con React + Tailwind CSS.{" "}
+          <a href="https://github.com/alex-boni/portfolio">Ver en GitHub</a>
+        </p>
         <SocialNetworks />
       </footer>
     </>
